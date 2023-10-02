ccd1e7a41900e38065cdb8fc0df474383a1a8def
Ashmikaverma <140064665+Ashmikaverma@users.noreply.github.com>
2023-10-02 20:55:50 +0530

    Add files via upload

---
+++ src/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Contact.css'
+const Contact=()=>{
+  return(
+    <>
+    <div className="lastpage">
+  <h1 className="lasthead">CONNECT WITH ME</h1>
+  <img src="https://imgs.search.brave.com/L6okuc3c98Z3uxIG9fOvUOd80m3NtR-1Nh3tKbEq7s4/rs:fit:560:320:1/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi85Lzk1L0lu/c3RhZ3JhbV9sb2dv/XzIwMjIuc3ZnLzUx/MnB4LUluc3RhZ3Jh/bV9sb2dvXzIwMjIu/c3ZnLnBuZw"height="30px" width="30px"/>
+  </div>
+  </>
+  )
+}
+export default Contact;
